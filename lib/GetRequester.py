22514b250518b582086608184c265fcb8878ff62
victor <victor.murithi@student.moringaschool.com>
2024-01-10 10:32:18 +0300

    added

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -16,3 +16,6 @@ class GetRequester:
         response_body = self.get_response_body()
         return json.loads(response_body)
         pass
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+get_requester.load_json()
